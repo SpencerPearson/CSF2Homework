959d1f4dfdf5af5c75b552aa24d4326905dc0143
Spencer Pearson <spencer.pearson.816@outlook.com>
2020-11-04 14:13:28 -0600

    Added Login class

---
+++ ClassesLibrary/Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Login
+    {
+        //fields
+        private string _username;
+        private string _password;
+
+        //properties
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public string Password
+
+
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        //ctors
+        public Login(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public Login() { }
+
+        //methods
+        public override string ToString()
+        {
+            return string.Format("username: {0}\npassword: {1}", Username, Password);
+        }
+    }
+}
