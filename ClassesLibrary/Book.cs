6f6e7f96cfa2568751b78533d6a6415bac0afaa4
Spencer Pearson <spencer.pearson.816@outlook.com>
2020-11-05 01:29:06 -0600

    Built out and tested Book class

---
+++ ClassesLibrary/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Book
+    {
+        //fields
+
+        //properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int NumberOfPages { get; set; }
+
+        //ctors
+        public Book(string title, string author, int numberOfPages)
+        {
+            Title = title;
+            Author = author;
+            NumberOfPages = numberOfPages;
+        }
+
+        //methods
+        public override string ToString()
+        {
+            return string.Format($"Title: \"{Title}\"\nAuthor: {Author}\nPages: {NumberOfPages}");
+        }
+    }
+}
