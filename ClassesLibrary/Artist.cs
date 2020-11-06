8fa8dbb0d7e55115ffe87b8498196cb5a36dde27
Spencer Pearson <spencer.pearson.816@outlook.com>
2020-11-05 23:31:15 -0600

    Built Artist class

---
+++ ClassesLibrary/Artist.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Artist
+    {
+        //fields
+
+        //props
+        public Song[] Tracks { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        //ctors
+        public Artist(Song[] tracks, string title, string genre)
+        {
+            Tracks = tracks;
+            Title = title;
+            Genre = genre;
+        }
+
+        //methods
+        public override string ToString()
+        {
+            return string.Format("Artist: {0} Genre: {1}", Title, Genre);
+        }
+    }
+}
